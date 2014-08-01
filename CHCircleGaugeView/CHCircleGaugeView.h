1244b6b591ad4e2609b89ad79f4b8a19e3f58451
Matthew Morey <me@matthewmorey.com>
2014-08-01 07:15:08 -0700

    Updated copyright to ChaiOne instead of Matthew Morey.

--- CHCircleGaugeView/CHCircleGaugeView.h
+++ CHCircleGaugeView/CHCircleGaugeView.h
@@ -1,7 +1,7 @@
 //
 //  CHCircleGaugeView.h
 //
-//  Copyright (c) 2014 Matthew Morey <me@matthewmorey.com>
+//  Copyright (c) 2014 ChaiOne <http://www.chaione.com/>
 //
 //  Permission is hereby granted, free of charge, to any person obtaining a copy
 //  of this software and associated documentation files (the "Software"), to deal
